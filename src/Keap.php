a2d79c42e705093b4369ef0a4dba83c1ffd6b3dc
Todd Stoker <sttodd@gmail.com>
2026-01-20 11:12:49 -0700

    for IDE autocomplete

--- src/Keap.php
+++ src/Keap.php
@@ -37,6 +37,7 @@ use Toddstoker\KeapSdk\Resources\ResourceFactory;
  * @method \Toddstoker\KeapSdk\Resources\V1\FilesResource|\Toddstoker\KeapSdk\Resources\V2\FilesResource files(?int $version = null)
  * @method \Toddstoker\KeapSdk\Resources\V1\HooksResource hooks(?int $version = null)
  * @method \Toddstoker\KeapSdk\Resources\V1\OpportunitiesResource opportunities(?int $version = null)
+ * @method \Toddstoker\KeapSdk\Resources\V2\OrdersResource orders(?int $version = null)
  * @method \Toddstoker\KeapSdk\Resources\V2\ReportsResource reports(?int $version = null)
  * @method \Toddstoker\KeapSdk\Resources\V2\SettingsResource settings(?int $version = null)
  * @method \Toddstoker\KeapSdk\Resources\V1\TagsResource|\Toddstoker\KeapSdk\Resources\V2\TagsResource tags(?int $version = null)
