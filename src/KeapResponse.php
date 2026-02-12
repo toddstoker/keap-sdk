3610cd78ca908abefd54cbe6b582f20e59ae282e
TWithers <timothy.withers@gmail.com>
2026-02-12 16:13:21 -0700

    Added ability (for debugging) to get the last request as a curl command

--- src/KeapResponse.php
+++ src/KeapResponse.php
@@ -5,6 +5,42 @@ namespace Toddstoker\KeapSdk;
 class KeapResponse extends \Saloon\Http\Response
 {
     /**
+     * Convert the PSR-7 request into a runnable curl command.
+     */
+    public function requestToCurlCommand(): string
+    {
+        $request = $this->getPsrRequest();
+
+        $method = $request->getMethod();
+        $url = (string) $request->getUri();
+        $parts = ['curl'];
+
+        if ($method !== 'GET') {
+            $parts[] = '-X '.escapeshellarg($method);
+        }
+
+        foreach ($request->getHeaders() as $name => $values) {
+            if (strtolower($name) === 'host') {
+                continue;
+            }
+
+            foreach ($values as $value) {
+                $parts[] = '-H '.escapeshellarg("{$name}: {$value}");
+            }
+        }
+
+        $body = (string) $request->getBody();
+
+        if ($body !== '') {
+            $parts[] = '-d '.escapeshellarg($body);
+        }
+
+        $parts[] = escapeshellarg($url);
+
+        return implode(" \\\n  ", $parts);
+    }
+
+    /**
      * Get the rate limit information from the response headers.
      *
      * @return array<string, array<string, int|\DateTimeImmutable>>
